6d0a56012a8bc9933844755a33f12b76455b44a0
NatalliaAkaronka <n.g.okoronko@mail.ru>
2021-04-20 23:01:14 +0300

    Добавьте файлы проекта.

---
+++ Lab16_Okoronko/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Lab16_Okoronko
+{
+    public delegate  void MyDelegate();
+    public class Ping
+    {
+        public event MyDelegate MyEvent;
+
+        public  void Hit()
+        {
+            Console.WriteLine("Ping recieved Pong");
+            MyEvent();
+
+        }
+
+    }
+    public class Pong
+    {
+        public event MyDelegate MyEvent;
+        public void Hit()
+        {
+            Console.WriteLine("Pong recieved Ping");
+            MyEvent();
+        }
+    }
+        class Program
+    {
+        static void Main()
+        {
+
+                Ping ping = new Ping();
+                Pong pong = new Pong();
+
+                ping.MyEvent += pong.Hit;
+                pong.MyEvent += ping.Hit;
+                ping.Hit();
+
+
+            Console.ReadLine();
+        }
+    }
+}
